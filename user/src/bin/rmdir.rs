1766cfb1ade51752684a3a5689e2b5befa0207c4
TD-Sky <three-dim-sky@foxmail.com>
2024-06-05 11:39:37 +0800

    bin: rmdir

--- user/src/bin/rmdir.rs
+++ user/src/bin/rmdir.rs
@@ -2,14 +2,14 @@
 #![no_main]
 #![feature(format_args_nl)]
 
-use user::fs::unlink;
+use user::fs::rmdir;
 use user::println;
 
 #[no_mangle]
 fn main(_argc: usize, argv: &[&str]) -> i32 {
     for path in &argv[1..] {
-        if unlink(path).is_none() {
-            println!("rm: {path} not found, or isn't file");
+        if rmdir(path).is_none() {
+            println!("rm: {path} not found, or isn't empty directory");
         }
     }
     0
